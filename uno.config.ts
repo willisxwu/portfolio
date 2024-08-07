2dfbccb8e44765de4e85df9cbd33d993a5c2f952
Willis <willisxwu@gmail.com>
2024-08-07 17:06:47 -0400

    chore: add breakpoint setting

--- uno.config.ts
+++ uno.config.ts
@@ -4,8 +4,18 @@ import {
   presetAttributify,
   transformerVariantGroup,
 } from 'unocss'
+import type { Theme } from 'unocss/preset-mini'
+import { Breakpoints } from './src/constant/breakpoint'
+
 
 export default defineConfig({
   presets: [presetUno(), presetAttributify()],
+  theme: <Theme>{
+    breakpoints: {
+      md: `${Breakpoints.md}px`,
+      lg: `${Breakpoints.lg}px`,
+      xl: `${Breakpoints.xl}px`,
+    },
+  },
   transformers: [transformerVariantGroup()],
 })
