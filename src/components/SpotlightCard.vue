1de97b054612f6156fff0fd696c3ac29f25d59d4
Willis <willisxwu@gmail.com>
2024-08-09 19:07:59 -0400

    feat: add SpotlightCard component

---
+++ src/components/SpotlightCard.vue
@@ -0,0 +1,24 @@
+<script setup lang="ts">
+const props = withDefaults(
+  defineProps<{
+    image: string
+    title: string
+    description: string
+  }>(),
+  {},
+)
+</script>
+
+<template>
+  <div class="p-5 bg-white rounded-2">
+    <img
+      class="max-w-20"
+      :src="props.image"
+      :alt="`${props.title.toLowerCase().replaceAll(' ', '-')}-icon`"
+    />
+    <div>
+      <p class="text-title-xl-lighter" v-text="props.title"></p>
+      <p class="mt-2 text-content-sm" v-text="props.description"></p>
+    </div>
+  </div>
+</template>
