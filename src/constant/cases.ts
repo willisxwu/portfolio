93a80cbfcf23086c81706c34f6c8c78f1e5b1f37
Willis <willisxwu@gmail.com>
2024-08-10 01:22:40 -0400

    feat: update cases config

--- src/constant/cases.ts
+++ src/constant/cases.ts
@@ -3,43 +3,42 @@ import cmsImage from '../assets/cms.jpg'
 import themedGameImage from '../assets/themed-game.jpg'
 import landingPageImage from '../assets/landingPages.jpg'
 import shippingRestrictionsImage from '../assets/shipping-restrictions.jpg'
+import type { ICaseInfo } from '../../types/caseIntro'
 
-interface ICaseItem {
-  handle: string
-  image: string
-  title: string
-  description: string
-}
-
-export const cases: ICaseItem[] = [
+export const cases: ICaseInfo[] = [
   {
     handle: 'membership-registration-bonus',
     image: membershipBonusImage,
     title: 'Membership Registration Bonus',
-    description: 'Developed a registration bonus feature for an online shopping website, offering a discount code to encourage first-time purchases.'
+    description: 'Developed a registration bonus feature for an online shopping website, offering a discount code to encourage first-time purchases.',
+    effect: 'Through the development of the registration bonus feature, we provided an incentive for new customers, thereby increasing the likelihood of initial purchases and fostering customer engagement right from the start.'
   },
   {
     handle: 'cms-for-content-management',
     image: cmsImage,
     title: 'CMS for Content Management',
-    description: 'Created a CMS feature for an online shopping website, enabling real-time content and asset adjustments.'
+    description: 'Created a CMS feature for an online shopping website, enabling real-time content and asset adjustments.',
+    effect: ''
   },
   {
     handle: 'themed-interactive-game-page',
     image: themedGameImage,
     title: 'Themed Interactive Game Page',
-    description: 'Developed a themed interactive game for a UK online shopping website, boosting product exposure and guiding new customer registrations.'
+    description: 'Developed a themed interactive game for a UK online shopping website, boosting product exposure and guiding new customer registrations.',
+    effect: ''
   },
   {
     handle: 'responsive-product-landing-pages',
     image: landingPageImage,
     title: 'Responsive Product Landing Pages',
-    description: 'Created responsive product landing pages for an online shopping website, effectively showcasing products.'
+    description: 'Created responsive product landing pages for an online shopping website, effectively showcasing products.',
+    effect: ''
   },
   {
     handle: 'region-specific-shipping-restrictions',
     image: shippingRestrictionsImage,
     title: 'Region-Specific Shipping Restrictions',
-    description: 'Implemented shipping restrictions for specific regions, ensuring compliance with authorized shipping areas.'
+    description: 'Implemented shipping restrictions for specific regions, ensuring compliance with authorized shipping areas.',
+    effect: ''
   }
 ]
