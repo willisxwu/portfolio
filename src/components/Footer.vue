9f667032876e939d9196c2c40f775bebc7b29863
Willis <willisxwu@gmail.com>
2024-08-10 22:48:05 -0400

    feat: add email contact, linkedin, github link on footer

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,7 +4,30 @@ const year = date.getFullYear()
 </script>
 
 <template>
-  <div class="pt-50 pb-5 flex flex-center text-gray-70">
+  <div class="pt-50 pb-5 flex flex-center gap-5 text-gray-70">
     <div>CHANG-FENG WU © {{ year }}</div>
+    <a href="mailto:willisxwu@gmail.com">Contact</a>
+    <a
+      class="flex flex-center"
+      href="https://www.linkedin.com/in/willisxwu/"
+      target="_blank"
+    >
+      <img
+        src="https://img.icons8.com/ios-filled/100/585858/linkedin.png"
+        class="w-5"
+        alt="linkedIn"
+      />
+    </a>
+    <a
+      class="flex flex-center"
+      href="https://github.com/willisxwu"
+      target="_blank"
+    >
+      <img
+        src="https://img.icons8.com/ios-filled/100/585858/github.png"
+        class="w-5"
+        alt="github-icon"
+      />
+    </a>
   </div>
 </template>
