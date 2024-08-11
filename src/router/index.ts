42b9c3ce69cb22539af242e908d399b3232343cc
Willis <willisxwu@gmail.com>
2024-08-11 17:38:28 -0400

    chore: configure base path for Vue Router

--- src/router/index.ts
+++ src/router/index.ts
@@ -2,7 +2,7 @@ import { createRouter, createWebHistory } from 'vue-router'
 import routes from './routes'
 
 const router = createRouter({
-  history: createWebHistory(),
+  history: createWebHistory('/portfolio/'),
   routes,
   scrollBehavior(_to, _from, savedPosition) {
     if (savedPosition) {
