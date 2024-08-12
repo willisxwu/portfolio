68e011a871c5206468f4cfa8894df85dc546dfd9
Willis <willisxwu@gmail.com>
2024-08-12 18:39:33 -0400

    feat: add mobile version for navigation RWD

--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,28 +1,35 @@
 <script setup lang="ts">
-import { ref, onMounted, onUnmounted } from 'vue'
+import { ref, onMounted, onUnmounted, watch, computed } from 'vue'
 import logo from '../assets/logo.svg'
 import { useRouter } from 'vue-router'
+import { useBreakpoints } from '../hooks/useBreakpoints'
 
 interface INavigationItem {
   path: string
   name: string
+  mobileTitle: string
 }
 
 const navigationItems: INavigationItem[] = [
   {
     path: '/',
     name: 'Home',
+    mobileTitle: "Willis's Portfolio",
   },
   {
     path: '/about',
     name: 'About',
+    mobileTitle: 'About Willis',
   },
   {
     path: '/works',
     name: 'Works',
+    mobileTitle: "Willis's Previous Works",
   },
 ]
 const { currentRoute } = useRouter()
+const { isMobile } = useBreakpoints()
+
 const isActive = (path: string): boolean => {
   if (path === '/works') {
     return currentRoute.value.path.startsWith('/works')
@@ -34,7 +41,12 @@ const isHidden = ref(false)
 let lastScrollTop = 0
 
 const handleScroll = () => {
+  if (isMenuOpen.value) {
+    return
+  }
+
   const currentScrollTop = document.documentElement.scrollTop
+
   if (currentScrollTop > lastScrollTop) {
     isHidden.value = true
   } else {
@@ -45,20 +57,53 @@ const handleScroll = () => {
 
 const getHeight = () => {
   const element = document.querySelector('.nav-container')
-  return element ? element.clientHeight + 35 : 0
+  const additionalHeight = isMobile.value ? 0 : 35
+
+  return element ? element.clientHeight + additionalHeight : 0
 }
 
 const emit = defineEmits<{
   (e: 'navigationHeight', value: number): void
 }>()
+const updateNavigationHeight = () => {
+  if (isMenuOpen.value) {
+    return
+  }
+
+  emit('navigationHeight', getHeight())
+}
 
 onMounted(() => {
   window.addEventListener('scroll', handleScroll)
-  emit('navigationHeight', getHeight())
+  window.addEventListener('resize', updateNavigationHeight)
+  updateNavigationHeight() // initial navigation height
 })
 
 onUnmounted(() => {
   window.removeEventListener('scroll', handleScroll)
+  window.removeEventListener('resize', updateNavigationHeight)
+})
+
+const isMenuOpen = ref(false)
+const toggleMenu = () => {
+  isMenuOpen.value = !isMenuOpen.value
+}
+
+watch(isMobile, (value: boolean) => {
+  if (!value) {
+    isMenuOpen.value = false
+  }
+})
+
+const currentMobileTitle = computed(() => {
+  const currentPath = currentRoute.value.path
+  const matchedItem = navigationItems.find((item) => {
+    if (item.path === '/works') {
+      return currentPath.startsWith('/works')
+    }
+    return item.path === currentPath
+  })
+  return matchedItem ? matchedItem.mobileTitle : ''
 })
 </script>
 
@@ -70,7 +115,9 @@ onUnmounted(() => {
       { 'translate-y-[-100%]': isHidden },
     ]"
   >
+    <!-- for tablet, desktop -->
     <div
+      v-show="!isMobile"
       class="mt-5 px-10 py-2.5 flex flex-center w-fit rounded-10 text-title-xl-lighter bg-white"
     >
       <router-link to="/" class="flex flex-center">
@@ -88,6 +135,45 @@ onUnmounted(() => {
           v-text="item.name"
         >
         </router-link>
+      </div>
+    </div>
+    <!-- for mobile -->
+    <div v-show="isMobile" class="w-full">
+      <div
+        class="flex justify-between w-full bg-white border-b-(solid 1.5px gray-10)"
+      >
+        <router-link to="/" class="p-4 flex flex-center">
+          <img :src="logo" class="w-6 aspect-ratio-1/1" alt="logo" />
+        </router-link>
+        <div class="flex justify-start items-center w-full">
+          <span
+            class="pl-4 text-(title-md gray-90) border-l-(solid 1.5px gray-10)"
+            v-text="currentMobileTitle"
+          ></span>
+        </div>
+        <img
+          class="p-4 w-8 aspect-ratio-1/1"
+          src="https://img.icons8.com/forma-regular-filled/100/2d2d2d/menu.png"
+          alt="menu-icon"
+          @click="toggleMenu"
+        />
+      </div>
+      <div
+        :class="[
+          'w-full bg-white text-(banner-md gray-90) bg-opacity-50 backdrop-blur-2xl overflow-hidden transition-[max-height] duration-250 ease-in-out	',
+          isMenuOpen ? 'max-h-100vh' : 'max-h-0',
+        ]"
+      >
+        <div class="px-19 pt-20 flex flex-col gap-10 h-100vh">
+          <router-link
+            v-for="item in navigationItems"
+            :key="item.path"
+            :to="item.path"
+            v-text="item.name"
+            @click="toggleMenu"
+          >
+          </router-link>
+        </div>
       </div>
     </div>
   </div>
