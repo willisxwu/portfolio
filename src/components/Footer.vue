fc33d137d788e346ca0d48d99c6dc407bfce3806
Willis <willisxwu@gmail.com>
2024-08-12 22:26:44 -0400

    feat: add mobile, tablet version for footer RWD

--- src/components/Footer.vue
+++ src/components/Footer.vue
@@ -4,30 +4,34 @@ const year = date.getFullYear()
 </script>
 
 <template>
-  <div class="pt-50 pb-5 flex flex-center gap-5 text-gray-70">
+  <div
+    class="pt-20 pb-5 flex flex-center flex-col-reverse gap-3 md:(pt-30 pb-10 flex-row gap-5) text-gray-70 lg:pt-50"
+  >
     <div>CHANG-FENG WU © {{ year }}</div>
-    <a href="mailto:willisxwu@gmail.com">Contact</a>
-    <a
-      class="flex flex-center"
-      href="https://www.linkedin.com/in/willisxwu/"
-      target="_blank"
-    >
-      <img
-        src="https://img.icons8.com/ios-filled/100/585858/linkedin.png"
-        class="w-5"
-        alt="linkedIn"
-      />
-    </a>
-    <a
-      class="flex flex-center"
-      href="https://github.com/willisxwu"
-      target="_blank"
-    >
-      <img
-        src="https://img.icons8.com/ios-filled/100/585858/github.png"
-        class="w-5"
-        alt="github-icon"
-      />
-    </a>
+    <div class="flex gap-5">
+      <a href="mailto:willisxwu@gmail.com">Contact</a>
+      <a
+        class="flex flex-center"
+        href="https://www.linkedin.com/in/willisxwu/"
+        target="_blank"
+      >
+        <img
+          src="https://img.icons8.com/ios-filled/100/585858/linkedin.png"
+          class="w-5"
+          alt="linkedIn"
+        />
+      </a>
+      <a
+        class="flex flex-center"
+        href="https://github.com/willisxwu"
+        target="_blank"
+      >
+        <img
+          src="https://img.icons8.com/ios-filled/100/585858/github.png"
+          class="w-5"
+          alt="github-icon"
+        />
+      </a>
+    </div>
   </div>
 </template>
