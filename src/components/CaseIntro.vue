82a63b47648d298f4db48ede144c7a0d4caffec4
Willis <willisxwu@gmail.com>
2024-08-16 20:09:14 -0400

    feat: update CaseIntro style and outcome section structure

--- src/components/CaseIntro.vue
+++ src/components/CaseIntro.vue
@@ -23,7 +23,7 @@ const props = withDefaults(
 
 <template>
   <div
-    class="px-6 pt-10 flex flex-col gap-10 md:(px-10 pt-5 gap-15) lg:(px-20 pt-10 gap-20)"
+    class="px-6 pt-10 flex flex-col gap-10 md:(px-10 pt-5 gap-15) lg:(mx-auto pt-10 max-w-330 gap-20)"
   >
     <!-- Case Head -->
     <div class="py-5 md:max-w-200 lg:max-w-250">
@@ -41,9 +41,7 @@ const props = withDefaults(
       ></p>
     </div>
     <!-- Case Lists -->
-    <div
-      class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:( max-w-330 grid-cols-3)"
-    >
+    <div class="grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
       <div
         class="p-5 bg-white rounded-2"
         v-for="list in props.caseLists"
@@ -64,9 +62,7 @@ const props = withDefaults(
         class="text-(content-md gray-70) md:max-w-200 lg:max-w-250"
         v-text="props.criteriaDescription"
       ></p>
-      <div
-        class="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:( max-w-330 grid-cols-3)"
-      >
+      <div class="mt-5 grid grid-cols-1 gap-5 md:grid-cols-2 lg:grid-cols-3">
         <SpotlightCard
           v-for="item in props.caseCriteria"
           :key="item.title"
@@ -89,29 +85,31 @@ const props = withDefaults(
       </div>
     </div>
     <!-- Case Outcome -->
-    <div v-if="props.caseOutcome">
-      <div class="mb-5 text-(headline-md gray-90)">Outcome</div>
-      <div class="flex flex-col gap-15">
-        <div v-for="item in props.caseOutcome" :key="item.title">
-          <p class="text-(content-sm gray-70)" v-text="item.title"></p>
-          <img
-            class="mt-3 w-full rounded-2 md:max-w-200"
-            :src="item.image"
-            alt="case-outcome-image"
-          />
+    <div>
+      <div v-if="props.caseOutcome">
+        <div class="mb-5 text-(headline-md gray-90)">Outcome</div>
+        <div class="flex flex-col gap-15">
+          <div v-for="item in props.caseOutcome" :key="item.title">
+            <p class="text-(content-sm gray-70)" v-text="item.title"></p>
+            <img
+              class="mt-3 w-full rounded-2 md:max-w-200"
+              :src="item.image"
+              alt="case-outcome-image"
+            />
+          </div>
         </div>
       </div>
+      <!-- For Slot -->
+      <div>
+        <slot />
+      </div>
+      <!-- Works Link -->
+      <router-link
+        to="/works"
+        class="mt-10 px-5 py-2 w-fit block text-(content-md-bolder gray-90) border-(solid 1.5px gray-90) rounded-10 trans-200 hover:(bg-gray-90 text-white)"
+      >
+        View More Works
+      </router-link>
     </div>
-    <!-- For Slot -->
-    <div>
-      <slot />
-    </div>
-    <!-- Works Link -->
-    <router-link
-      to="/works"
-      class="px-4 py-1 w-fit block bg-gray-70 text-white text-content-md-bolder rounded-1.5 trans-200 hover:(bg-gray-50 text-white)"
-    >
-      View More Works
-    </router-link>
   </div>
 </template>
